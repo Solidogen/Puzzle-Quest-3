3d6b2b48cccf44b8cdff53ca9d2e71f9674c906f
solidogen <solidogen@gmail.com>
2019-04-13 00:23:17 +0200

    Second loop clean

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -22,35 +22,33 @@ public class Board : MonoBehaviour
 
     private void Setup()
     {
-        for (int i = 0; i < width; i++)
+        doForEveryDot((i, j) =>
         {
-            for (int j = 0; j < height; j++)
+            var cellName = $"( {i}, {j} )";
+
+            var tempPosition = new Vector2(i, j);
+            var backgroundTile = Instantiate(tilePrefab, tempPosition, Quaternion.identity);
+            backgroundTile.name = "bg";
+
+            int dotToUse = Random.Range(0, availableDotTypes.Length);
+
+            int maxIterations = 0;
+            while (MatchesAt(i, j, availableDotTypes[dotToUse]) && maxIterations < 100)
             {
-                var cellName = $"( {i}, {j} )";
-
-                var tempPosition = new Vector2(i, j);
-                var backgroundTile = Instantiate(tilePrefab, tempPosition, Quaternion.identity);
-                backgroundTile.name = "bg";
-
-                int dotToUse = Random.Range(0, availableDotTypes.Length);
-
-                int maxIterations = 0;
-                while(MatchesAt(i, j, availableDotTypes[dotToUse]) && maxIterations < 100)
-                {
-                    dotToUse = Random.Range(0, availableDotTypes.Length);
-                    maxIterations++;
-                    Debug.Log(maxIterations);
-                }
-
-                GameObject dot = Instantiate(availableDotTypes[dotToUse], tempPosition, Quaternion.identity);
-                dot.transform.parent = transform;
-                dot.name = "dot" + cellName;
-                backgroundTile.transform.parent = dot.transform;
-
-                allDotsOnBoard[i, j] = dot;
+                dotToUse = Random.Range(0, availableDotTypes.Length);
+                maxIterations++;
+                Debug.Log(maxIterations);
             }
-        }
+
+            GameObject dot = Instantiate(availableDotTypes[dotToUse], tempPosition, Quaternion.identity);
+            dot.transform.parent = transform;
+            dot.name = "dot" + cellName;
+            backgroundTile.transform.parent = dot.transform;
+
+            allDotsOnBoard[i, j] = dot;
+        });
     }
+
 
     private bool MatchesAt(int column, int row, GameObject gameObject)
     {
@@ -73,17 +71,17 @@ public class Board : MonoBehaviour
             {
                 if (allDotsOnBoard[column, row - 1].tag == gameObject.tag &&
                     allDotsOnBoard[column, row - 2].tag == gameObject.tag)
-                    {
-                        return true;
-                    }
+                {
+                    return true;
+                }
             }
             if (column > 1)
             {
                 if (allDotsOnBoard[column - 1, row].tag == gameObject.tag &&
                     allDotsOnBoard[column - 2, row].tag == gameObject.tag)
-                    {
-                        return true;
-                    }
+                {
+                    return true;
+                }
             }
         }
         return false;
@@ -101,7 +99,8 @@ public class Board : MonoBehaviour
 
     public void DestroyAllMatches()
     {
-        doForEveryDot((i, j) => {
+        doForEveryDot((i, j) =>
+        {
             DestroyMatchesAt(i, j);
         });
     }
