c4e30fab1bd04ab48b95aac135288b5980954bd6
solidogen <solidogen@gmail.com>
2019-04-12 21:58:06 +0200

    Moving pieces

--- Assets/Scripts/Dot.cs
+++ Assets/Scripts/Dot.cs
@@ -5,34 +5,112 @@ using UnityEngine;
 
 public class Dot : MonoBehaviour
 {
+    public int column;
+    public int row;
+    public int targetX;
+    public int targetY;
+    private Board board;
+    private GameObject otherDot;
     private Vector2 firstTouchPosition;
     private Vector2 finalTouchPosition;
-    public float swipeAngle = 0;
+    private Vector2 tempPosition;
+    public float radianAngle = 0;
 
     void Start()
     {
-        
+        board = FindObjectOfType<Board>();
+        targetX = (int)transform.position.x;
+        column = targetX;
+        targetY = (int)transform.position.y;
+        row = targetY;
     }
 
     void Update()
     {
-        
+        targetX = column;
+        targetY = row;
+        // X
+        tempPosition = new Vector2(targetX, transform.position.y);
+        if (Mathf.Abs(targetX - transform.position.x) > 0.1f)
+        {
+            // move towards the target
+            transform.position = Vector2.Lerp(transform.position, tempPosition, 0.4f);
+        }
+        else
+        {
+            // directly set the position
+            transform.position = tempPosition;
+            board.allDotsOnBoard[column, row] = gameObject;
+        }
+        // Y
+        tempPosition = new Vector2(transform.position.x, targetY);
+        if (Mathf.Abs(targetY - transform.position.y) > 0.1f)
+        {
+            // move towards the target
+            transform.position = Vector2.Lerp(transform.position, tempPosition, 0.4f);
+        }
+        else
+        {
+            // directly set the position
+            transform.position = tempPosition;
+            board.allDotsOnBoard[column, row] = gameObject;
+        }
     }
 
     private void OnMouseDown() {
         firstTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Debug.Log(firstTouchPosition);
     }
 
     private void OnMouseUp() {
         finalTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Debug.Log(finalTouchPosition);
         CalculateAngle();
     }
 
     private void CalculateAngle()
     {
-        swipeAngle = Mathf.Atan2(finalTouchPosition.y - firstTouchPosition.y, finalTouchPosition.x - firstTouchPosition.x) * 180 / Mathf.PI;
-        Debug.Log(swipeAngle);
+        radianAngle = Mathf.Atan2(finalTouchPosition.y - firstTouchPosition.y, finalTouchPosition.x - firstTouchPosition.x) * 180 / Mathf.PI;
+        CheckAngle();
+    }
+
+    private void CheckAngle()
+    {
+        var swipeAngle = SwipeHelpers.getSwipeAngleFromRadian(radianAngle);
+        Debug.Log($"Swipe {swipeAngle}");
+        SwapGemIfPossible(swipeAngle);
+    }
+
+    private void SwapGemIfPossible(SwipeAngle swipeAngle)
+    {
+        switch (swipeAngle)
+        {
+            case SwipeAngle.Right:
+                if (column < board.width) {
+                    otherDot = board.allDotsOnBoard[column + 1, row];
+                    otherDot.GetComponent<Dot>().column--;
+                    column++;
+                }
+                break;
+            case SwipeAngle.Up:
+                if (row < board.height) {
+                    otherDot = board.allDotsOnBoard[column, row + 1];
+                    otherDot.GetComponent<Dot>().row--;
+                    row++;
+                }
+                break;
+            case SwipeAngle.Left:
+                if (column > 0) {
+                    otherDot = board.allDotsOnBoard[column - 1, row];
+                    otherDot.GetComponent<Dot>().column++;
+                    column--;
+                }
+                break;
+            case SwipeAngle.Down:
+                if (row > 0) {
+                    otherDot = board.allDotsOnBoard[column, row - 1];
+                    otherDot.GetComponent<Dot>().row++;
+                    row--;
+                }
+                break;
+        }
     }
 }
---
+++ Assets/Scripts/Enums/SwipeAngle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SwipeAngle
+{
+    Up,
+    Down,
+    Left,
+    Right
+}
+
+public class SwipeHelpers
+{
+    public static SwipeAngle getSwipeAngleFromRadian(float radian)
+    {
+        switch (radian)
+        {
+            case float r when r > -45 && r <= 45: return SwipeAngle.Right;
+            case float r when r > 45 && r <= 135: return SwipeAngle.Up;
+            case float r when r > 135 || r <= -135: return SwipeAngle.Left;
+            case float r when r < -45 && r >= -135: return SwipeAngle.Down;
+            default: throw new InvalidOperationException();
+        }
+    }
+}
