db68b7c426dc8c56580e68e16498482ed50040b2
solidogen <solidogen@gmail.com>
2019-04-12 17:25:42 +0200

    Skrypty

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class Board : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var asd = "ASD:";
+        var random = new Random().Next();
+        var iss = random.Equals(asd);
+        var sd = this.name.ToString();
+    }
+}
